9b78158bd429773a48aa625a06dcb3d8bdc36d9a
Lily Lane <sublimelab@sublime.cool>
2026-05-13 14:06:20 -0400

    Update cal_data_import.bat

--- cal_data_import.bat
+++ cal_data_import.bat
@@ -1,2 +1,2 @@
-2>error_cal_data_import.txt C:\Users\christopher.martin\AppData\Local\Continuum\anaconda3\python.exe C:\Users\christopher.martin\Documents\Python\cal_data_processing\cal_data_import.py %*
+2>error_cal_data_import.txt python cal_data_import.py %*
 pause
